229362ce8bae4eaa4a66ae9067bac26977016e26
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-12-05 10:28:54 +0530

    [amol]- Flip the Coin and Head or Tail

---
+++ BasicCore_Programing/BasicCore_Programing/LeapYear.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCore_Programing
+{
+    internal class LeapYear
+    {
+    }
+}
--- BasicCore_Programing/BasicCore_Programing/Program.cs
+++ BasicCore_Programing/BasicCore_Programing/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using BasicCore_Programing;
+{
+    while (true)
+    {
+        Console.WriteLine("1. FlipCoin");
+        int option = Convert.ToInt32(Console.ReadLine());
+        switch (option)
+        {
+            case 1:
+                FlipCoin flipcoin = new FlipCoin();
+                flipcoin.CheckHeadTail();
+                break;
+            default:
+                Console.WriteLine("Please enter valid choice");
+                break;
+        }
+    }
+}
+ 
