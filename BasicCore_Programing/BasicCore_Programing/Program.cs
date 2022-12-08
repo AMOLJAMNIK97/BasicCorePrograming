e37174b2e26a3ff1317c4a2a912dd76b92fb2082
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-12-08 17:25:20 +0530

    [amol]- Harmonic Nth Value

--- BasicCore_Programing/BasicCore_Programing/Program.cs
+++ BasicCore_Programing/BasicCore_Programing/Program.cs
@@ -6,7 +6,7 @@ using System.Security.Cryptography.X509Certificates;
     
         while (true)
         {
-            Console.WriteLine("1.FlipCoin 2.\n Enter year 3.\n Enter a Power");
+            Console.WriteLine("1.FlipCoin 2.\n Enter year 3.\n Enter a Power\n 4. nthHarmonic");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -22,6 +22,10 @@ using System.Security.Cryptography.X509Certificates;
                 Power power = new Power();
                 power.FindPower();
                     break;
+            case 4:
+                Harmonic harmonic = new Harmonic();
+                harmonic.FindHarmoic();
+                break;
                 default:
                     Console.WriteLine("Please enter valid choice");
                     break;
